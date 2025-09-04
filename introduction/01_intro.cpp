52dbfd777010c69f1df6098428e9ff5e8290efbc
sssctkt <justaskaisssctkt@gmail.com>
2025-09-04 08:38:06 -0400

    first_program

--- introduction/01_intro.cpp
+++ introduction/01_intro.cpp
@@ -1,1 +1,13 @@
-//This is the first introduction of c++
+#include <iostream>
+using namespace std;
+//you have to keep in mind, you have to use precise language
+//keyword
+int main()
+{
+    //first statement--cout, (output)
+    //you must have opening and closing quotations
+    //all statement must end with ;
+    cout<<"hello";
+
+    return 0;
+}
