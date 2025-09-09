01cc1e5967c3638b117acbdd1920ced897da64aa
sssctkt <justaskaisssctkt@gmail.com>
2025-09-09 10:25:20 -0400

    exercise 2 q2 answer

--- variables/main.cpp
+++ variables/main.cpp
@@ -1,18 +1,17 @@
 
-
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    /*1.	Create 3 declarations with 3 different types, 
-    and 3 initializations with 3 different types*/
-    int x;
-    float y;
-    double z;
+    /*2.	List out the restrictions of creating variables*/
     
-    int a=12;
-    float v=3.1;
-    double q=4.56;
+    /*
+    1. number cannot go first
+    2. NO KEYWORDS
+    3.Has to be one word 
+    
+    below are suggestions to follow
+    1. try to use lower case letters (uppercase is for constant)
+    2. try to be explantory (instead of x1234, use the word like radius)
     return 0;
 }
