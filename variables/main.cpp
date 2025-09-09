d4c34b40c41a0da892a9775b668edd399cbded22
sssctkt <justaskaisssctkt@gmail.com>
2025-09-09 09:49:12 -0400

    Rules of creating variables

---
+++ variables/main.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //variable
+    //= assignment symbol (from right to left)
+    //type
+    
+    //a variable is to store and manipulate values
+    //in c++ , whenever you create a variable, you have to associate with type
+
+
+    //types: int (integer, no decimal)
+    //float-->(decimal, up to 6 decimal places)
+    //double-->(decimal, up to 9 decimal places)
+    //char-->(one character. Eg, &, u)
+    //string-->(collection of characters. Eg: how are you doing??)
+    
+    //how to create a variable
+    //first of all variable cannot start with numbers, 
+    //has to be one phrase
+    
+    int abdul; //this is declaration of an integer variable
+    abdul=21;
+    cout<<abdul;
+    return 0;
+}
