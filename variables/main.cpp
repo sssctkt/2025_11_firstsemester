e8b2cdc17a8fb906e9c0a6a56b031a22f1c8bda1
sssctkt <justaskaisssctkt@gmail.com>
2025-09-09 10:35:55 -0400

    exercise 2 q3 answer (improved version)

--- variables/main.cpp
+++ variables/main.cpp
@@ -3,15 +3,10 @@
 using namespace std;
 int main()
 {
-    /*2.	List out the restrictions of creating variables*/
-    
-    /*
-    1. number cannot go first
-    2. NO KEYWORDS
-    3.Has to be one word 
-    
-    below are suggestions to follow
-    1. try to use lower case letters (uppercase is for constant)
-    2. try to be explantory (instead of x1234, use the word like radius)
+    double temp; //create a variable
+    cout<<"Enter the temperature in Fahrenheit: ";
+    cin>>temp; //collect input and assign to the variable temp
+    temp=(5.0/9)*(temp-32); //integer division!!
+    cout<<"The temperature in Celsius is: "<<temp;
     return 0;
 }
