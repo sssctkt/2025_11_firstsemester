83fbc64369e7491776dad9c4b5c1063eb1b88642
sssctkt <justaskaisssctkt@gmail.com>
2025-09-11 10:09:22 -0400

    swapping variables

--- variables/main.cpp
+++ variables/main.cpp
@@ -4,14 +4,21 @@
 using namespace std;
 int main()
 {
-    string s1="simon";
-    string s2="heri";
+    string s1, s2, s3;//s3 is the temporary variables
+    cout<<"Enter the first message: ";
+    getline(cin, s1); //get the first message
+    cout<<"Enter the second message: ";
+    getline(cin, s2); //get the second message
     
+    s3=s1;
     s1=s2;
-    s2="Anoop";
+    s2=s3;
+    //do your work here
+    //swap the message
+    //you can but you can only create one extra variables
     
-    cout<<s1;
-    //at this level, if you do not see the variable gets changed directly
-    //it will remain the same
+    
+    cout<<s1<<endl; //it should print s2
+    cout<<s2<<endl; //it should print s1
     return 0;
 }
