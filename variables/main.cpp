f8f2fa0d374272e0a402bc81220046651921aa7d
sssctkt <justaskaisssctkt@gmail.com>
2025-09-12 08:48:44 -0400

    math operator

--- variables/main.cpp
+++ variables/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
+    //math operator
+    cout<<3+4<<endl; //addition
+    cout<<5-2<<endl; //subtraction
+    cout<<9*2<<endl; //multiplication
+    //be careful of the following
     
-    float x=10.5, y=4;
-    x=x+y;
-    cout<<x<<" "<<y;
+    cout<<-11.0/2<<endl; //division
+    //be careful of integer division
+    //integer division-->if both numbers are integer (no decimal)
+    //answer from division will be without decimal
+    //to counter that, you need to put a decimal back to 
+    //one of the number
     
+    cout<<23%4<<endl; //remainder of 5/4
     return 0;
 }
