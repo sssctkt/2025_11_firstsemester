5bd93ac68892e0af6adb045ed94d2b8a317125e7
sssctkt <justaskaisssctkt@gmail.com>
2025-09-12 08:30:51 -0400

    exercise 2 question 4 answer

--- variables/main.cpp
+++ variables/main.cpp
@@ -1,24 +1,14 @@
+/*
+4.	What is an assignment symbol? 
+Give one example of correct assignment, 
+and one example of incorrect assignment.
+*/
 
-#include <iostream>
-#include <string> //collection of sentence
-using namespace std;
-int main()
-{
-    string s1, s2, s3;//s3 is the temporary variables
-    cout<<"Enter the first message: ";
-    getline(cin, s1); //get the first message
-    cout<<"Enter the second message: ";
-    getline(cin, s2); //get the second message
-    
-    s3=s1;
-    s1=s2;
-    s2=s3;
-    //do your work here
-    //swap the message
-    //you can but you can only create one extra variables
-    
-    
-    cout<<s1<<endl; //it should print s2
-    cout<<s2<<endl; //it should print s1
-    return 0;
-}
+/*
+The assignment symbol is "="
+
+x=5    -correct assignment
+5=x    -incorrect assignment
+Assignment is always from right to left
+
+*/
