25693abeb298749b0fb665e3057e3916a701a8c8
sssctkt <justaskaisssctkt@gmail.com>
2025-09-15 10:07:21 -0400

    math operators

---
+++ operators/main.cpp
@@ -0,0 +1,13 @@
+//math operator
+/*
++ addition
+- subtraction
+* multiplication
+/ division
+% modulus-->the remainder of a/b For example 13%3 is 1 
+
+integer division--if both numbers have no decimals, the answer will have no decimals
+ex: 5/3 will become 1 
+To counter that put ".0" on one or both of them
+5.0/3
+*/
