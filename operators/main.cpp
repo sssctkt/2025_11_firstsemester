651da8d0865bd330388c967344d7e366debf65e6
sssctkt <justaskaisssctkt@gmail.com>
2025-09-25 09:46:07 -0400

    application of comparison operators

--- operators/main.cpp
+++ operators/main.cpp
@@ -1,37 +1,24 @@
-//more on operators
 
 #include <iostream>
 using namespace std;
 int main()
 {
-    //review on math operators
-    //+ addition
-    //- subtraction
-    //* multiplication
-    // / division
-    // % modulus
-
-    //review on compound operators
-    // +=   a+=b same as a=a+b
-    // -=   a-=b same as a=a-b
-    // *=   a*=b same as a=a*(b)
-    // /=   a/=b same as a=a/b
-    
     //comparision operators
-    // > greater than
-    // < less than
-    // >= greater than or equal to (do not put =>)
-    // <= less than or equal to (do not put =<)
-    // == equal 
-    // != not equal (do not put !==)
-    
-    
-    //bool-->boolean (true (1) or false (0))
-    
-    bool result;
-    result=6!=11;
-    
-    cout<<result;
+    // > < >=, <=, ==, !=
+    int number;
+    cin>>number;
+    if (number%2==0){//multiple of 2 (even number)
+         cout<<"It is an even number"<<endl;
+    }
+    if (number%2!=0){
+        cout<<"It is an odd number"<<endl;
+    }
+    if (number%11==0){
+        cout<<"it is a multiple of 11"<<endl;
+    }
+    if (number%5!=0){
+        cout<<"it is not a multiple of 5"<<endl;
+    }
 
     return 0;
 }
