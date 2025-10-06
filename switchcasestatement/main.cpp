f60cc8dc9af4c2544805d4b4a98f8f1cf0033212
sssctkt <justaskaisssctkt@gmail.com>
2025-10-06 09:06:11 -0400

    switch case statement for characters

--- switchcasestatement/main.cpp
+++ switchcasestatement/main.cpp
@@ -8,14 +8,14 @@ int main()
     //switch case is very similar to if and else statement 
     //but switch case doesn't work for > <, >=, etc 
     //only for equal 
-    int level;
+    char level;
     cout<<"Enter your level: ";
     cin>>level;
     switch(level){
-        case 4:cout<<"A";break;
-        case 3:cout<<"B";break;
-        case 2:cout<<"C";break;
-        case 1:cout<<"D";break;
+        case 'A':cout<<"good";break;
+        case 'B':cout<<"ok";break;
+        case 'C':cout<<"ooooooooooook";break;
+        case 'D':cout<<"not good";break;
         default:cout<<"you fail!";break;
     }
     
