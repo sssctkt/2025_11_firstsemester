03afa9f6d6f131313b11f8bf5ac08db792c57555
sssctkt <justaskaisssctkt@gmail.com>
2025-10-09 09:54:23 -0400

    another confusion for if else

--- ifelsestatements/main.cpp
+++ ifelsestatements/main.cpp
@@ -7,13 +7,21 @@ int main()
     //why do we need else if and else-->pick one and discards the other
     int mark;
     cout<<"Enter your mark";
-    cin>>ph;
-    if (ph<2){
-        cout<<"it is very acidic";
+    cin>>mark;
+    if (mark>=80 && mark<=100){
+        cout<<"A"<<endl;
     }
-    else if (ph<7){
-        cout<<"acidic";
+    if (mark>=70 && mark<=80){
+        cout<<"B"<<endl;
     }
-    //if you only put if statement, it will check every case that fits
+    if (mark>=60 && mark<=70){
+        cout<<"C"<<endl;
+    }
+    if (mark>=50 && mark<=60){
+        cout<<"D"<<endl;
+    }
+    else{
+        cout<<"Fail"<<endl;
+    }
     return 0;
 }
