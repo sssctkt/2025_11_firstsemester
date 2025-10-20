5a8eaf5d8300b7b2037d08bee4cfc0b9e9708b58
sssctkt <justaskaisssctkt@gmail.com>
2025-10-20 08:47:40 -0400

    "FAIR" random number generator

--- 06_loops/while_loops/while_loops.cpp
+++ 06_loops/while_loops/while_loops.cpp
@@ -1,18 +1,18 @@
 
 #include <iostream>
+#include <ctime> //for time 
+#include <cstdlib> // for rand()
 using namespace std;
 int main()
-{
-    /*According to structred program theorem, it says that if you know:
-    1. sequence (next step)
-    2. selection (if else)
-    3. iteration (loops)
-    you can solve any solvable prograamming questions*/
-    int start=1; //step 1 
-    while (start<=10){ //step 2: condition
-       cout<<start<<endl;
-       start+=1; //step 3: modify the value such that it has a chance to exit    
+{   //goal: create a "FAIR" random number generator
+    
+    //how you start a random value
+    srand(time(NULL));
+    int random=1;
+    while(random!=4){
+        random=rand()%20+1; //between 1 and 20;
+        cout<<random<<endl;
     }
-    cout<<"done"<<endl;
+    cout<<random;
     return 0;
 }
