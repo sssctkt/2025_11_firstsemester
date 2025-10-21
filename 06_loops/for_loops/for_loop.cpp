e9d5e320f1a51b7fb43d3dc9cfd56291ac3c4cae
sssctkt <justaskaisssctkt@gmail.com>
2025-10-21 10:35:41 -0400

    Create for_loop.cpp

---
+++ 06_loops/for_loops/for_loop.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{   //task for today: the first example has been changed into for loop
+    //change the 2nd one into for loop as well
+    
+    int starting=1; //1 starting
+    while(starting<=10){ //2 condition
+        cout<<starting<<endl;
+        starting=starting+1;//3 give the loop a chance to exit
+    }
+    for (int abdul=1; abdul<=10; abdul=abdul+1){ //1;//2;//3
+        cout<<abdul<<endl;
+    }
+    /*
+    //starting at 10
+    //count down to 0
+    int starting=10; //1 starting
+    while(starting>=0){ //2 condition
+        cout<<starting<<endl;
+        starting=starting-2;//3 give the loop a chance to exit
+    }
+    
+    */
+    
+    
+    
+    
+    
+
+    return 0;
+}
