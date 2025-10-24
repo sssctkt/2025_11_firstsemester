b6da6adad1c2d7854414549af3f1a68052c4f31e
sssctkt <justaskaisssctkt@gmail.com>
2025-10-24 08:48:21 -0400

    continue and while loop

--- 06_loops/continue.cpp
+++ 06_loops/continue.cpp
@@ -9,12 +9,22 @@ int main()
     
     //break is to get out of the loop it is in (only the inner it is in)
     //continue is to go back to the loop control mechanism
-    for (int i=0; i<20; i+=1){//-<loop control mechanism
+    int i=0;
+    while(i<20){
+        
+        if(i==13){
+            i+=1;
+            continue;
+        }
+        cout<<i<<endl;
+        i+=1;
+    }//continue will skip the rest, so the code will be forever stuck at 12
+    /*for (int i=0; i<20; i+=1){//-<loop control mechanism
         if (i==13){
             continue;
         }
         cout<<i<<endl;
     }
-    //in for loop, increment/decrement is the last thing to happen
+    //in for loop, increment/decrement is the last thing to happen*/
     return 0;
 }
