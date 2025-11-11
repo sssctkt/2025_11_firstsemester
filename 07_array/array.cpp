891542960690d478d67d7c57b7dcfb0ccf54176d
sssctkt <justaskaisssctkt@gmail.com>
2025-11-11 10:14:08 -0500

    Create array.cpp

---
+++ 07_array/array.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //array -->is to store many elements of the same type 
+    int a[10]={11, 22, 33, 44, 55, 66, 77, 88, 99, 109}; //length is 5 
+    for (int i=9; i>=0; i--){
+        cout<<a[i];
+    }
+    
+    
+    //create your own array, content 10;
+    //print everything backward-->but is there an easier way to do this
+    return 0;
+}
