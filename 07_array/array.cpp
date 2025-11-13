9ba9c42c7f9aab3047fc82554b1ce4d9932a4e63
sssctkt <justaskaisssctkt@gmail.com>
2025-11-13 10:13:55 -0500

    Update array.cpp

--- 07_array/array.cpp
+++ 07_array/array.cpp
@@ -1,20 +1,13 @@
 
 #include <iostream>
+#define length sizeof(a)/sizeof(a[0])
 using namespace std;
 int main()
 {
-    int a[10]={1,2,2}; //this is the only line that [10] is the length
-    //all other times, [] is the index
-    //for number array, if you have less number initialized then the length
-    //the rest of the content will become zero
-    char sentence[50]="hello!";
-    //for number array
-    cout<<a<<endl; //do not do this for number array
-    cout<<sentence<<endl; //character array is an exception
-    //in order to print the number array, we got to use a loop to print them
-    //one by one
-    for (int i=0; i<9; i++){
-        cout<<a[i]<<endl;
+    int a[5]={0, 2, 5, 8, 12}; //this is the only line that [10] is the length
+    
+    for (int i=0; i<length; i++){
+        cout<<a[i]<<" ";
     }
     return 0;
 }
