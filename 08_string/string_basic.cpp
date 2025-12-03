e8fa17b247be491bd54edddb1712fc964f605f09
sssctkt <justaskaisssctkt@gmail.com>
2025-12-03 10:08:03 -0500

    extending the string

--- 08_string/string_basic.cpp
+++ 08_string/string_basic.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 int main()
 {
-    //string 
-    //what is a string-->string is a collection of characters
-    
-    //most of the time you will be using new and easy method to deal with string  
-    string s; //string class
-    int length;
-    //string class also gives us very easy way to manipulate the string 
-    //1. find the length of the string 
-    getline(cin, s);
-    length=s.size(); //how you find the length of the string (starts at 1)
-    cout<<length;
+    //string can also be extended
+    string s="This class is good", s2="not";
+   // cout<<s.size();
+    //cout<<s[0];
+    s+=s2;
+    cout<<s<<endl;
+    cout<<s2<<endl;
+    s+='a'; //if you see '' not a string, but just a character 
+    cout<<s<<endl;
+    // '' is a character 
+    // " " is a string
     return 0;
 }
