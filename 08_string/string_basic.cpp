f5e32e4628222c495b4495f0ea5aa23739bff1c1
sssctkt <justaskaisssctkt@gmail.com>
2025-12-04 08:38:18 -0500

    string copy

--- 08_string/string_basic.cpp
+++ 08_string/string_basic.cpp
@@ -2,18 +2,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main()
 {
-    //string can also be extended
-    string s="This class is good", s2="not";
-   // cout<<s.size();
-    //cout<<s[0];
-    s+=s2;
-    cout<<s<<endl;
+    string s1="I'm going home!", s2;
+    //the length of the string
+    int length=s1.size();
+    cout<<length<<endl;
+    //If the length is 15, index 0-14
+    cout<<s1[14]<<endl;
+    //string class can also expand the string (character or string)
+    getline(cin, s2);
     cout<<s2<<endl;
-    s+='a'; //if you see '' not a string, but just a character 
-    cout<<s<<endl;
-    // '' is a character 
-    // " " is a string
+    s1+=s2; //s1=s1+s2
+    cout<<s1<<endl;
+    
+    //New day, you can also copy string and replace it
+    s1='b';
+    cout<<s1<<endl;
+    
     return 0;
 }
